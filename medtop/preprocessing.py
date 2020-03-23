5167ec3b167812a0e03df3cc72195929eee5af87
Evan French <etfrench@vcu.edu>
2020-03-23 10:27:51 -0400

    Extract most representative phrase from sentences

--- medtop/preprocessing.py
+++ medtop/preprocessing.py
@@ -6,6 +6,7 @@ __all__ = ['decontracted', 'token_filter', 'tokenize_and_stem', 'tokenize_and_st
 import nltk
 from nltk.tokenize import WhitespaceTokenizer
 from nltk.corpus import stopwords
+import numpy as np
 import string
 import re
 
@@ -59,30 +60,40 @@ def tokenize_and_stem(text):
     stop_words = set(stopwords.words('english')) | custom_stop_words
     table  = str.maketrans(' ', ' ', string.punctuation+"“"+"”")
     sent = nltk.sent_tokenize(text)
-    split_sent = []
-    raw_sent = []
+    sent_tokens = []
+    sent_pos = []
+    sent_raw = []
 
     #For each sentence in document get back the list of tokenized words with contractions normalized and punctuation removed
     for s in sent:
         # Condense whitespace
         s = re.sub("\s+", " " ,s).strip()
 
-        raw_sent.append(s)
+        sent_raw.append(s)
         tokenized = WhitespaceTokenizer().tokenize(decontracted(s).translate(table))
+
+        # Part of Speech tags
+        pos_tags = nltk.pos_tag(tokenized)
 
         # Lemmatize and convert to lowercase
         lemma = [lemmatizer.lemmatize(t).lower() for t in tokenized]
 
-        # Filter out stopwords (including some punctuation) and tokens with no alpha characters
-        filtered_tokens = [token for token in lemma if token_filter(token, stop_words)]
-        split_sent.append(filtered_tokens)
+        # Create a filter to remove stopwords (including some punctuation) and tokens with no alpha characters
+        filter_mask = [token_filter(token, stop_words) for token in lemma]
 
-    return split_sent, raw_sent
+        # Add filtered list of tokens and PoS tags
+        filtered_tokens = list(np.array(lemma)[filter_mask])
+        filtered_pos = list(np.array(pos_tags)[filter_mask])
+        sent_tokens.append(filtered_tokens)
+        sent_pos.append(filtered_pos)
+        break
+
+    return sent_tokens, sent_pos, sent_raw
 
 # Cell
 def tokenize_and_stem_original(text):
     """
-    Original method which parses POS and LOC. `tokenize_and_stem` is a stripped down version of this. \n
+    Original method which parses LOC. `tokenize_and_stem` is a stripped down version of this. \n
     Parse out sentences, remove contractions, tokenize by white space, and remove all punctuation, and lemmatize tokens
     """
     lemmatizer = nltk.WordNetLemmatizer()
